51462b709fbb546903e9fb953435a37ce317c715
alstj2384 <ass91160@naver.com>
2023-12-28 12:13:34 +0900

    구간합구하기5_dp로 수정

--- src/백준/자바/구간합구하기5/Main.java
+++ src/백준/자바/구간합구하기5/Main.java
@@ -4,52 +4,16 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    // 이 부분에 static으로 사용 할 멤버 선언
-    // private int max = 0; ...
-    // private int[] arr; ...
-    // private void dfs()...
     private int n, m;
     private int[][] arr;
-    class point{
-        int x;
-        int y;
+    private int[][] d;
 
-        public point(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-        public String toString(){
-            return "("+x+","+y+")";
-        }
-    }
-    class pair{
-        point a;
-        point b;
-
-        public pair(point a, point b) {
-            this.a = a;
-            this.b = b;
-        }
-        public String toString(){
-            return a + " "+ b;
-        }
-    }
-
-    int seq_sum(pair p){
-        int sum = 0;
-        for(int i = p.a.x-1; i < p.b.x ; i++)
-            for(int j = p.a.y-1; j < p.b.y; j++)
-                sum += arr[i][j];
-
-        return sum;
-    }
     public void solution() throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
-        Vector<pair> v = new Vector<>();
 
         arr = new int[n][n];
 
@@ -58,6 +22,12 @@ public class Main {
             for(int j = 0; j < n; j++)
                 arr[i][j] = Integer.parseInt(st.nextToken());
         }
+        d = new int[n+1][n+1];
+
+        for(int i = 1; i < n+1; i++){
+            for(int j = 1; j < n+1; j++)
+                d[i][j] = d[i-1][j] + d[i][j-1] - d[i-1][j-1] + arr[i-1][j-1];
+        }
 
         for(int i = 0; i < m; i++){
             st = new StringTokenizer(br.readLine());
@@ -65,12 +35,9 @@ public class Main {
             int y1 = Integer.parseInt(st.nextToken());
             int x2 = Integer.parseInt(st.nextToken());
             int y2 = Integer.parseInt(st.nextToken());
-            v.add(new pair(new point(x1, y1), new point(x2, y2)));
+            System.out.println(d[x2][y2] - (d[x1-1][y2] + d[x2][y1-1]-d[x1-1][y1-1]) );
         }
 
-        for(var k : v){
-            System.out.println(seq_sum(k));
-        }
     }
     public static void main(String[] args) throws Exception{
         new Main().solution();
