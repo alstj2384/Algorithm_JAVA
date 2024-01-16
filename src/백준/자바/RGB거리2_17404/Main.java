8d4f00198f9ba6715f50224d7bbd933ebbff1220
alstj2384 <ass91160@naver.com>
2024-01-16 15:09:13 +0900

    RGB거리2_17404

--- src/백준/자바/RGB거리2_17404/Main.java
+++ src/백준/자바/RGB거리2_17404/Main.java
@@ -1,7 +1,6 @@
 package 백준.자바.RGB거리2_17404;
 
 import java.io.*;
-import java.nio.Buffer;
 import java.util.*;
 
 public class Main {
@@ -18,24 +17,31 @@ public class Main {
         }
 
         int total = Integer.MAX_VALUE;
+
+        // R, G, B 각각에 대해 한 번씩 반복
         for(int i =0 ; i < 3; i++){
+            // i == 0인 경우, R 이외의 G, B는 각각 최댓값 + 1로 초기화
             for(int k = 0; k < 3; k++){
                 if(i == k) d[0][k] = RGB[0][k];
                 else d[0][k] = 1001;
             }
 
+            // dp
             for(int j = 1; j < N; j++){
                 d[j][0] = Math.min(d[j-1][1], d[j-1][2])+RGB[j][0];
                 d[j][1] = Math.min(d[j-1][0], d[j-1][2])+RGB[j][1];
                 d[j][2] = Math.min(d[j-1][0], d[j-1][1])+RGB[j][2];
             }
 
+            // 처음 고정 시켜놓은 값의 인덱스는 참조가 불가능하므로
+            // 해당 인덱스를 제외한 다른 인덱스의 최소값 결정
             int t = Integer.MAX_VALUE;
             for(int k = 0; k < 3; k++){
                 if(i==k) continue;
                 if(t > d[N-1][k]) t = d[N-1][k];
             }
 
+            // 총 3번의 반복(R, G, B) 중 최소값을 갱신
             total = Math.min(t, total);
         }
 
