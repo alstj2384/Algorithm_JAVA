857b800a9d76c36894a233bd32f4a54d85a2352a
alstj2384 <ass91160@naver.com>
2024-01-18 13:06:41 +0900

    용액_2467

---
+++ src/백준/자바/용액_2467/Main.java
@@ -0,0 +1,46 @@
+package 백준.자바.용액_2467;
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    private int a, b, p, q, k;
+    public void solution() throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        k = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int[] arr = new int[k];
+        for(int i = 0 ; i < k; i++)
+            arr[i] = Integer.parseInt(st.nextToken());
+
+
+        p = 0;
+        q = k-1;
+        int min = Integer.MAX_VALUE;
+
+        // 탐색
+        while(p < q){
+            int sum = arr[p] + arr[q];
+            if(min > Math.abs(sum)){
+                min = Math.abs(sum);
+                a = arr[p];
+                b = arr[q];
+            }
+            if(sum > 0) q--;
+            else if(sum < 0) p++;
+            else{
+                a = arr[p];
+                b = arr[q];
+                break;
+            }
+        }
+
+        System.out.println(a + " " + b);
+        br.close();
+    }
+
+
+    public static void main(String[] args) throws Exception{
+        new Main().solution();
+    }
+}
