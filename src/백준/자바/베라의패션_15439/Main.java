b390b9aeeba5bb19c0211bbf9a228c2acb803c27
alstj2384 <ass91160@naver.com>
2024-02-16 20:21:03 +0900

    베라의패션_15439

---
+++ src/백준/자바/베라의패션_15439/Main.java
@@ -0,0 +1,21 @@
+package 백준.자바.베라의패션_15439;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public void solution() throws Exception {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        System.out.println(N*(N-1));
+
+
+    }
+
+
+    public static void main(String[] args) throws Exception{
+        new Main().solution();
+    }
+}
