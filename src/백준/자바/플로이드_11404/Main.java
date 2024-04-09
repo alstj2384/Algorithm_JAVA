03af9bb8fae5314006d0a783ae71cce81098ebb1
alstj2384 <ass91160@naver.com>
2024-04-09 20:36:20 +0900

    플로이드_11404(re)

--- src/백준/자바/플로이드_11404/Main.java
+++ src/백준/자바/플로이드_11404/Main.java
@@ -4,48 +4,50 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    private int n, m;
-    private int[][] arr;
-
-    void floyd(){
-        for(int k = 1; k < n+1; k++)
-            for(int a = 1; a < n +1; a++)
-                for(int b = 1; b < n+1; b++)
-                    arr[a][b] = Math.min(arr[a][b] , arr[a][k] + arr[k][b]);
-    }
-
-    public void solution() throws Exception{
+    public void solution() throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        n = Integer.parseInt(br.readLine());
-        m = Integer.parseInt(br.readLine());
-        arr = new int[n+1][n+1];
-        for(int i = 1; i < n+1; i++){
-            for(int j = 1; j < n+1; j++){
-                arr[i][j] = 10000001;
-                if(i == j) arr[i][j] = 0;
-            }
+
+        int N = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
+
+        int[][] floyd = new int[N+1][N+1];
+
+        for(int i = 0; i < N+1; i++){
+            Arrays.fill(floyd[i], 100_000_000);
+            floyd[i][i] = 0;
         }
-        for(int i = 0; i < m; i++){
-            st = new StringTokenizer(br.readLine());
+
+        for(int i = 0; i < M; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
             int c = Integer.parseInt(st.nextToken());
 
-            if(arr[a][b] > c) arr[a][b] = c;
+            floyd[a][b] = Math.min(floyd[a][b], c);
         }
-
-        floyd();
-
-        for(int i = 1; i < n+1; i++){
-            for(int j = 1; j < n+1; j++) {
-                if(arr[i][j] == 10000001)
-                    System.out.print(0 + " ");
-                else System.out.print(arr[i][j] + " ");
+        // k, a, b = 1 to n+1
+        for(int k = 1; k < N+1; k++){
+            for(int a = 1; a < N+1; a++){
+                for(int b = 1; b < N+1; b++){
+                    floyd[a][b] = Math.min(floyd[a][b], floyd[a][k] + floyd[k][b]);
+                }
             }
-            System.out.println();
         }
+        StringBuilder sb = new StringBuilder();
+        for(int i = 1; i < N+1; i++){
+            for(int j = 1; j < N+1; j++){
+                if(floyd[i][j] == 100_000_000) sb.append("0 ");
+                else sb.append(floyd[i][j] + " ");
+            }
+            sb.append('\n');
+        }
+
+        System.out.print(sb);
+
+
     }
+
+
     public static void main(String[] args) throws Exception{
         new Main().solution();
     }
