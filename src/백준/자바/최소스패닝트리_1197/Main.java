1038de6ba6bfde58d602660559b66317cdee23e4
alstj2384 <ass91160@naver.com>
2024-04-09 21:03:17 +0900

    최소스패닝트리_1197

--- src/백준/자바/최소스패닝트리_1197/Main.java
+++ src/백준/자바/최소스패닝트리_1197/Main.java
@@ -1,78 +1,72 @@
 package 백준.자바.최소스패닝트리_1197;
-
 import java.io.*;
 import java.util.*;
 
 public class Main {
-
-    class node implements Comparable<node>{
-        int s, e, v;
-        public node(int s, int e, int v){
-            this.s = s;
-            this.e = e;
+    class Node{
+        int u;
+        int v;
+        int w;
+        public Node(int u , int v, int w){
+            this.u = u;
             this.v = v;
-        }
-        @Override
-        public int compareTo(node o){
-            return this.v-o.v;
+            this.w = w;
         }
     }
 
     int[] parent;
-    int find(int x){
-        if(parent[x] == x)
-            return x;
+
+    private int find(int x){
+        if(parent[x] == x) return x;
         return parent[x] = find(parent[x]);
     }
 
-    void union(int a, int b){
-        int parentA = find(a);
-        int parentB = find(b);
+    private void union(int a, int b){
+        a = find(a);
+        b = find(b);
 
-        if(parentA != parentB){
-            parent[parentB] = parentA;
-        }
-    }
-
-    boolean isSameParent(int x, int y){
-        x = find(x);
-        y = find(y);
-        return x==y;
+        if(a != b) parent[b] = a;
     }
 
     public void solution() throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         int V = Integer.parseInt(st.nextToken());
         int E = Integer.parseInt(st.nextToken());
 
-        // parent 변수 초기화
-        parent = new int[V + 1];
-        for(int i = 1; i < V + 1; i++){
-            parent[i] = i;
-        }
+        List<Node> graph = new ArrayList<>();
 
-        Queue<node> pq = new PriorityQueue<>();
+        parent = new int[V+1];
+        for(int i = 0; i < V+1; i++) parent[i] = i;
+
         for(int i = 0; i < E; i++){
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-            int w = Integer.parseInt(st.nextToken());
-            pq.add(new node(a, b, w));
+            int c = Integer.parseInt(st.nextToken());
+
+            graph.add(new Node(a, b, c));
         }
 
-        // 가중치에 맞게 정렬
+        Collections.sort(graph, new Comparator<Node>() {
+            @Override
+            public int compare(Node o1, Node o2) {
+                return o1.w - o2.w;
+            }
+        });
 
         int weight = 0;
-        for(int i = 0; i < E; i++){
-            node n = pq.poll();
-            if(!isSameParent(n.s, n.e)){
-                weight += n.v;
-                union(n.s, n.e);
+        for(int i = 0; i < graph.size(); i++){
+            Node next = graph.get(i);
+            if(find(next.u) != find(next.v)){
+                union(next.u, next.v);
+                weight += next.w;
             }
         }
+
         System.out.println(weight);
+
     }
 
 
