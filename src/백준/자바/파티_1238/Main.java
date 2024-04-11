3ca163473a4c2470524f4190efda469385cfba50
alstj2384 <ass91160@naver.com>
2024-04-11 22:45:19 +0900

    파티_1238

--- src/백준/자바/파티_1238/Main.java
+++ src/백준/자바/파티_1238/Main.java
@@ -4,42 +4,46 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    private int N, M, X;
     public void solution() throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-        int X = Integer.parseInt(st.nextToken())-1;
 
-        int[][] arr = new int[N][N];
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        X = Integer.parseInt(st.nextToken());
 
-        // 각 노드 최댓값으로 초기화
-        for(int i = 0; i < N; i++){
-            Arrays.fill(arr[i], 1_000_000_000);
-            arr[i][i] = 0;
+        int[][] map = new int[N+1][N+1];
+
+        for(int i = 0; i < N+1; i++){
+            Arrays.fill(map[i], 100_000_000);
+            map[i][i] = 0;
         }
 
-        // 입력 받기
-        for(int i = 0; i < M; i++){
+
+        for(int i = 0 ; i < M; i++){
             st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken())-1;
-            int b = Integer.parseInt(st.nextToken())-1;
-            arr[a][b] = Integer.parseInt(st.nextToken());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            int dist = Integer.parseInt(st.nextToken());
+
+            map[start][end] = dist;
         }
 
-        // 플로이드 워셜 알고리즘 수행
-        for(int i = 0; i < N; i++){
-            for(int a = 0; a < N; a++){
-                for(int b = 0; b < N; b++)
-                    arr[a][b] = Math.min(arr[a][b], arr[a][i]+arr[i][b]);
+        // 플로이드 워셜
+        for(int k = 1; k < N+1; k++){
+            for(int a = 1; a < N+1; a++){
+                for(int b = 1; b < N+1; b++){
+                    map[a][b] = Math.min(map[a][b], map[a][k]+map[k][b]);
+                }
             }
         }
+        int result = 0;
+        for(int i = 1; i < N+1; i++){
+            result = Math.max(result, map[i][X] + map[X][i]);
+        }
 
-        int max = 0;
-        for(int i = 0; i < N; i++)
-            max = Math.max(arr[i][X] + arr[X][i], max);
-        System.out.println(max);
-
+        System.out.println(result);
     }
 
 
