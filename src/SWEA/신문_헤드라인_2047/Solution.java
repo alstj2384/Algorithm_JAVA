8b3db0faf7fdfd603e0f96037c34d414c495577d
alstj2384 <ass91160@naver.com>
2024-05-12 20:35:45 +0900

    신문 헤드라인 2047

--- src/SWEA/신문_헤드라인_2047/Solution.java
+++ src/SWEA/신문_헤드라인_2047/Solution.java
@@ -1,2 +1,14 @@
-package SWEA.신문_헤드라인_2047;public class Solution {
+package SWEA.신문_헤드라인_2047;
+
+import java.util.Scanner;
+import java.io.FileInputStream;
+
+class Solution
+{
+    public static void main(String args[]) throws Exception
+    {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        System.out.println(s.toUpperCase());
+    }
 }
