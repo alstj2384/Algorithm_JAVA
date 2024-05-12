74d32e89b2f948f8cc6ef5fe79483f65de6491e2
alstj2384 <ass91160@naver.com>
2024-05-12 20:27:21 +0900

    평균값 구하기 2071

--- src/SWEA/평균값_구하기_2071/Solution.java
+++ src/SWEA/평균값_구하기_2071/Solution.java
@@ -1,2 +1,21 @@
-package SWEA.평균값_구하기_2071;public class Solution {
+package SWEA.평균값_구하기_2071;
+import java.util.Scanner;
+import java.io.FileInputStream;
+
+class Solution
+{
+    public static void main(String args[]) throws Exception
+    {
+        Scanner sc = new Scanner(System.in);
+        int T;
+        T=sc.nextInt();
+        for(int test_case = 1; test_case <= T; test_case++)
+        {
+            double sum = 0;
+            for(int i = 0; i < 10; i++){
+                sum += sc.nextDouble();
+            }
+            System.out.println("#"+test_case+" "+Math.round(sum/10));
+        }
+    }
 }
