25e07e468ef328a2782b1ef762f17b1dcd854e9f
alstj2384 <ass91160@naver.com>
2024-12-23 15:10:12 +0900

    이동하기_11048

---
+++ src/백준/자바/이동하기_11048/Main.java
@@ -0,0 +1,36 @@
+package 백준.자바.이동하기_11048;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[][] map = new int[N+1][M+1];
+
+        for(int i = 1; i <= N; i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j = 1; j <= M; j++){
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for(int i = 1; i <= N; i++){
+            for(int j = 1; j <= M; j++){
+                map[i][j] = Math.max(Math.max(map[i-1][j], map[i][j-1]), map[i-1][j-1]) + map[i][j];
+            }
+        }
+
+        System.out.println(map[N][M]);
+
+    }
+
+}
