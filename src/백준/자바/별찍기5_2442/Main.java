66775b96b87738e9058df570bdfcc4f6acb446b9
alstj2384 <ass91160@naver.com>
2025-01-16 12:56:33 +0900

    별찍기5 2442

---
+++ src/백준/자바/별찍기5_2442/Main.java
@@ -0,0 +1,27 @@
+package 백준.자바.별찍기5_2442;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        for(int i = 1; i <= N; i++){
+            for(int j = N-i; j > 0; j--){
+                System.out.print(" ");
+            }
+            for(int j = 1; j < i; j++){
+                System.out.print("*");
+            }
+            System.out.print("*");
+            for(int j = 1; j < i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
