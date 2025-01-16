6e83b7b6d34277cca3cf5205ca3c133e1b0c927c
alstj2384 <ass91160@naver.com>
2025-01-16 12:23:33 +0900

    그대로 출력하기 11718

---
+++ src/백준/자바/그대로출력하기_11718/Main.java
@@ -0,0 +1,16 @@
+package 백준.자바.그대로출력하기_11718;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String s;
+        while((s = br.readLine()) != null){
+            System.out.println(s);
+        }
+    }
+}
